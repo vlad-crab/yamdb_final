11934394aa8cd609d6cd63fd06bf4b05905e33b4
Владислав Казаков <vladikazakov05@gmail.com>
2023-04-20 21:42:21 +0300

    Надеюсь последняя версия

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 import api.views as views
 
-
 router_v1 = DefaultRouter()
 router_v1.register("titles", views.TitlesViewSet, basename='Title')
 router_v1.register("genres", views.GenreViewSet, basename='Genre')
