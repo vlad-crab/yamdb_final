02d956f3c337de3cd01bc731f2ef5e2979a02b3a
Владислав Казаков <vladikazakov05@gmail.com>
2023-04-27 16:34:47 +0300

    убрал статику из gitignore

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'reviews.User'
