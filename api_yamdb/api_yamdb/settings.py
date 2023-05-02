a9b23c72c8523b9134c36d46d84786e7ffe0658f
Владислав Казаков <vladikazakov05@gmail.com>
2023-05-02 22:55:43 +0300

    наконец разобрался с ошибкой в базе данных

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='api_yamdb'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456'),
         'HOST': os.getenv('DB_HOST', default='db'),
